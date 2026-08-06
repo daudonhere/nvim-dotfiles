0e11514d883bebc89a6435305369178010f67ae6
daudonhere <hellodadedaud@gmail.com>
2026-08-07 04:02:57 +0700

    revert: disable onedarkpro transparency

--- lua/plugins/appearance/theme.lua
+++ lua/plugins/appearance/theme.lua
@@ -5,7 +5,7 @@ return {
     config = function()
       require("onedarkpro").setup({
         options = {
-          transparency = true,
+          transparency = false,
           terminal_colors = true,
           highlight_inactive_windows = false,
         },
